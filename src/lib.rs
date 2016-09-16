b256f74181bb1ef7d43680f7daee9c00b916b8c0
Songbird0 <chaacygg@gmail.com>
2016-09-17 01:53:13 +0200

    0.2.1:Rédaction de la documentation de chaque macro.

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,18 @@
 /// 
 /// Le pack de loggers contient le module:
 /// 
-/// - proce
-/// - oop
+/// - [`proce`]
+/// - [`oop`]
+/// 
+/// [`proce`]: proce/
+/// [`oop`]: oop/
 /// 
 /// # EN:
 /// 
 /// The loggers pack contains this mods:
 /// 
-/// - proce
-/// - oop
+/// - [`proce`]
+/// - [`oop`]
 /// 
 pub mod loggers_pack
 {
@@ -22,6 +25,23 @@ pub mod loggers_pack
 	pub mod proce
 	{
 		#[macro_export]
+		/// # FR:
+		/// 
+		/// La macro `log!` est une amorçe permettant d'encapsuler des manipulations inutiles (concaténation manuelle).
+		/// log! amorce les macros suivantes:
+		/// 
+		/// - [`info!()`]
+		/// - [`warn!()`]
+		/// - [`error!()`]
+		/// - [`success!()`]
+		/// 
+		/// 
+		/// [`info!()`]: macro.info!.html
+		/// [`warn!()`]: macro.warn!.html
+		/// [`error!()`]: macro.error!.html
+		/// [`success!()`]: macro.success!.html
+		/// 
+		/// Ces dernières servent elles-même d'amorçes pour les fonctions du package `loggers_pack::oop`.
 		macro_rules! log {
 
 			($struct_name:expr, $log_type:expr, $message_content:expr, $variable:expr) => 
@@ -45,24 +65,88 @@ pub mod loggers_pack
 			};
 		}
 		#[macro_export]
+		/// #FR:
+		/// ## Dépendences:
+		/// 
+		/// - [`log!()`]
+		/// 
+		/// Exemple d'utilisation:
+		/// 
+		/// ```Rust
+		/// 
+		/// info!("Hello world! :D"); // => [INFO]: Hello world! :D
+		/// info!("Hello ", "world! :D"); // => [INFO]: Hello world! :D
+		/// info!("@StructName", "Hello ", "world! :D"); // => @StructName[INFO]: Hello world! :D
+		/// 
+		/// ```
+		/// 
+		/// [`log!()`]: macro.log!.html  
 		macro_rules! info {
 			($message_content:expr) => (log!("[INFO]:", $message_content));
 			($message_content:expr, $variable:expr) => (log!("[INFO]:", $message_content, $variable));
 			($struct_name:expr, $message_content:expr, $variable:expr) => (log!($struct_name, "[INFO]:", $message_content, $variable));
 		}
 		#[macro_export]
+		/// #FR:
+		/// ## Dépendences:
+		/// 
+		/// - [`log!()`]
+		/// 
+		/// Exemple d'utilisation:
+		/// 
+		/// ```Rust
+		/// 
+		/// warn!("Hello world! :D"); // => [WARNING]: Hello world! :D
+		/// warn!("Hello ", "world! :D"); // => [WARNING]: Hello world! :D
+		/// warn!("@StructName", "Hello ", "world! :D"); // => @StructName[WARNING]: Hello world! :D
+		/// 
+		/// ```
+		/// 
+		/// [`log!()`]: macro.log!.html  
 		macro_rules! warn {
 			($message_content:expr) => (log!("[WARNING]:", $message_content));
 			($message_content:expr, $variable:expr) => (log!("[WARNING]:", $message_content, $variable));
 			($struct_name:expr, $message_content:expr, $variable:expr) => (log!($struct_name, "[WARNING]:", $message_content, $variable));
 		}
 		#[macro_export]
+		/// #FR:
+		/// ## Dépendences:
+		/// 
+		/// - [`log!()`]
+		/// 
+		/// Exemple d'utilisation:
+		/// 
+		/// ```Rust
+		/// 
+		/// error!("Hello world! :D"); // => [ERROR]: Hello world! :D
+		/// error!("Hello ", "world! :D"); // => [ERROR]: Hello world! :D
+		/// error!("@StructName", "Hello ", "world! :D"); // => @StructName[ERROR]: Hello world! :D
+		/// 
+		/// ```
+		/// 
+		/// [`log!()`]: macro.log!.html  
 		macro_rules! error {
 			($message_content:expr) => (log!("[ERROR]:", $message_content));
 			($message_content:expr, $variable:expr) => (log!("[ERROR]:", $message_content, $variable));
 			($struct_name:expr, $message_content:expr, $variable:expr) => (log!($struct_name, "[ERROR]:", $message_content, $variable));
 		}
 		#[macro_export]
+		/// #FR:
+		/// ## Dépendences:
+		/// 
+		/// - [`log!()`]
+		/// 
+		/// Exemple d'utilisation:
+		/// 
+		/// ```Rust
+		/// 
+		/// success!("Hello world! :D"); // => [SUCCESS]: Hello world! :D
+		/// success!("Hello ", "world! :D"); // => [SUCCESS]: Hello world! :D
+		/// success!("@StructName", "Hello ", "world! :D"); // => @StructName[SUCCESS]: Hello world! :D
+		/// 
+		/// ```
+		/// 
+		/// [`log!()`]: macro.log!.html  
 		macro_rules! success {
 			($message_content:expr) => (log!("[SUCCESS]:", $message_content));
 			($message_content:expr, $variable:expr) => (log!("[SUCCESS]:", $message_content, $variable));
@@ -71,6 +155,7 @@ pub mod loggers_pack
 	}
 
     /// Contient les outils orientés objet
+    /// 
     /// Contains OO(object oriented) tools
     /// 
     /// ----
@@ -80,15 +165,14 @@ pub mod loggers_pack
     /// ### Attention:
     /// 
     /// Les *wrappers* (fonctions) supporteront l'intégralité des paramètres que peuvent capturer les macros;
-    /// Vous devrez donc veillez à laisser une chaîne de caractères vide là où vous ne souhaitez pas renseigner d'informations.
+    /// Vous devrez donc veiller à laisser une chaîne de caractères vide là où vous ne souhaitez pas renseigner d'informations.
     /// 
     /// Exemple:
     /// 
-    /// ```Rust 
-    /// 
+    /// ``` 
     /// struct A;
     /// 
-    /// use loggers_pack::oop::Logger;
+    /// use loggers_pack::oop::*;
     /// 
     /// impl Logger for A 
     /// {/* Les fonctions sont implémentées */}
@@ -102,12 +186,31 @@ pub mod loggers_pack
     /// ```
 	pub mod oop
 	{
-		/// Utilise 
+		/// Chaque fonction a été volontairement écrite de manière à être *indépendante* de chaque instance
+		/// de la structure.
+		/// Cela permettra de pouvoir dégager rapidement les informations que pourraient renvoyer les instances de cette dernière. 
 		pub trait Logger
 		{
-			fn info()
+			/// # info
+			/// 
+			fn info(struct_name: &str, message_content: &str, variable: &str) -> ()
 			{
+				info!(struct_name, message_content, variable);
+			}
 
+			fn warn(struct_name: &str, message_content: &str, variable: &str) -> ()
+			{
+				warn!(struct_name, message_content, variable);
+			}
+
+			fn error(struct_name: &str, message_content: &str, variable: &str) -> ()
+			{
+				error!(struct_name, message_content, variable);
+			}
+
+			fn success(struct_name: &str, message_content: &str, variable: &str) -> ()
+			{
+				success!(struct_name, message_content, variable);
 			}
 		}
 
@@ -161,5 +264,16 @@ mod proce_tests
 #[cfg(test)]
 mod oo_tests
 {
+	struct Alice;
+	use loggers_pack::oop::Logger;
+	impl Logger for Alice{/*...*/}
 
+	#[test]
+	fn pack_logger_oop()
+	{
+		Alice::info("@Alice", "Hello, I'm Alice ", "Peterson !");
+		Alice::warn("@Alice", "Hello, I'm Alice ", "Peterson !");
+		Alice::error("@Alice", "Hello, I'm Alice ", "Peterson !");
+		Alice::success("@Alice", "Hello, I'm Alice ", "Peterson !");
+	}
 }
